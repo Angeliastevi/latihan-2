3d074fa1bf7216eb851f35901a6fa5da1862ab6e
compac cq 43 <compac cq 43@DESKTOP-10I32SP>
2018-08-16 19:31:42 +0700

    Coba method length & isEmpty

--- CobaOperasiStri/src/Jobsheet11/CobaOperasiString.java
+++ CobaOperasiStri/src/Jobsheet11/CobaOperasiString.java
@@ -13,5 +13,14 @@ public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Angelia Stevinesyabudi Leoninda / X PRL 2 / 06";
         System.out.println("Identitas : "+ identitas);
+         String x = "operasi";
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
     }
 }
