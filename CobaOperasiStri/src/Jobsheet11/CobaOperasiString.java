a731ad86017e1901fe717038e8eede389d4ee067
compac cq 43 <compac cq 43@DESKTOP-10I32SP>
2018-08-16 19:24:43 +0700

    Coba method compareTo & compareToIgnoreCesa

---
+++ CobaOperasiStri/src/Jobsheet11/CobaOperasiString.java
@@ -0,0 +1,17 @@
+
+package Jobsheet11;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+
+    /**
+     * @param args the command line arguments
+     */
+    
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        String identitas = "Angelia Stevinesyabudi Leoninda / X PRL 2 / 06";
+        System.out.println("Identitas : "+ identitas);
+    }
+}
