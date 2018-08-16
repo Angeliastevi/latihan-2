16399674cf17106757d6a06663590d2f92524e42
compac cq 43 <compac cq 43@DESKTOP-10I32SP>
2018-08-16 21:02:15 +0700

    Initial Commit

---
+++ ConvertVocal2Angka/src/Jobsheet11/ConvertVocal2Angka.java
@@ -0,0 +1,19 @@
+package Jobsheet11;
+
+
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    
+      private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    public static void main(String[] args){
+    
+        String identitas = " Angelia Stevinesyabudi Leoninda / X RPL 2 / 06 ";
+        
+        tampilJudul(identitas); 
+    }
+}
